63b2dd1805ef035a6e9d34d9a561d6e7288a0bc4
gabrielsdinizbrq <gabrielsdiniz@brq.com>
2020-08-06 14:29:58 -0300

    Header update

--- theme/packages/bios/src/components/header.js
+++ theme/packages/bios/src/components/header.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { connect, styled } from "frontity";
 import { H1, H2 } from "./text/titles";
-import Container from "../assets/styles/general";
 import Link from "./link";
 import Nav from "./nav";
 import MobileMenu from "./menu";
@@ -23,6 +22,17 @@ const Header = ({ state }) => {
 
 // Connect the Header component to get access to the `state` in it's `props`
 export default connect(Header);
+
+const Container = styled.div`
+  width: 90%;
+  max-width: 100%;
+  box-sizing: border-box;
+  padding: 24px;
+  color: #fff;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+`;
 
 const StyledLink = styled(Link)`
   text-decoration: none;
