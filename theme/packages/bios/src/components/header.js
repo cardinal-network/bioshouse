41208d2e49263c9309cfe27269d9fd27a0b0d45c
gabrielsdinizbrq <gabrielsdiniz@brq.com>
2020-08-06 13:32:25 -0300

    General Style update

--- theme/packages/bios/src/components/header.js
+++ theme/packages/bios/src/components/header.js
@@ -3,6 +3,7 @@ import { connect, styled } from "frontity";
 import Link from "./link";
 import Nav from "./nav";
 import MobileMenu from "./menu";
+import Container from "../assets/styles/general-styles";
 
 const Header = ({ state }) => {
   return (
@@ -21,17 +22,6 @@ const Header = ({ state }) => {
 
 // Connect the Header component to get access to the `state` in it's `props`
 export default connect(Header);
-
-const Container = styled.div`
-  width: 90%;
-  max-width: 100%;
-  box-sizing: border-box;
-  padding: 24px;
-  color: #fff;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-around;
-`;
 
 const Title = styled.h2`
   margin: 0;
