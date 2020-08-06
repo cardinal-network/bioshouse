6256ebe0f3a900fdfef9477d839bf0a68c94734d
gabrielsdinizbrq <gabrielsdiniz@brq.com>
2020-08-06 14:24:51 -0300

    Header update

---
+++ theme/packages/bios/src/assets/styles/general.js
@@ -0,0 +1,12 @@
+import { styled } from "frontity";
+
+export const Container = styled.div`
+  width: 90%;
+  max-width: 100%;
+  box-sizing: border-box;
+  padding: 24px;
+  color: #fff;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+`;
